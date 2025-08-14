fbd8228c210c4fe0dbc404e03ac60c9979b774f8
Volker Kuntner <git.dsea@tutanota.com>
2025-08-14 18:43:50 +0200

    merge main

--- includes/scenes/scene_3d_game.h
+++ includes/scenes/scene_3d_game.h
@@ -5,19 +5,10 @@
 #define MAX_SCREEN_HEIGHT 900
 #define CURSOR_RADIUS 2
 
-<<<<<<< HEAD
-#define PLAYGROUND_Y_MAX 1
-#define PLAYGROUND_X_MAX 300
-#define PLAYGROUND_Z_MAX 300
-
-#define TARGET_FPS 820
-=======
 #define TARGET_FPS 4000
->>>>>>> main
 #define BLOCK_SIZE 1.0f
 #define HALF_BLOCK_SIZE (BLOCK_SIZE / 2.0f)
 #define MAX_PLACMENT_DISTANCE 1000.0
-#define PLAYER_REACH 8.0f
 
 int Scene3DGame(void);
 
--- src/scenes/scene_3d_game.c
+++ src/scenes/scene_3d_game.c
@@ -1,13 +1,8 @@
 #include "scenes/scene_3d_game.h"
 #include "core/voxel.h"
 #include "sprites/player.h"
-<<<<<<< HEAD
-#include "utils/mesher.h"
-#include "utils/performance.h"
-=======
 #include "utils/rescource_tracker.h"
 #include <raylib.h>
->>>>>>> main
 
 int Scene3DGame(void) {
 
@@ -19,64 +14,25 @@ int Scene3DGame(void) {
   DisableCursor();
   SetTargetFPS(TARGET_FPS);
   Player player = InitPlayer();
-<<<<<<< HEAD
-
-  Block blocks[PLAYGROUND_X_MAX][PLAYGROUND_Y_MAX][PLAYGROUND_Z_MAX] = {0};
-  InitBlocks(blocks);
-
-  Texture2D texture = LoadTexture("assets/log.png");
-  Model model = {0};
-  bool mesh_is_dirty = true;
-=======
   Voxel voxels[VOXEL_XYZ] = {0};
   InitVoxel(voxels);
->>>>>>> main
 
   while (!WindowShouldClose()) {
     StartPerformanceTracker("CompleteLoop");
-
-    if (mesh_is_dirty) {
-      UnloadModel(model);
-      Mesh mesh = GenerateGreedyMesh(blocks);
-      model = LoadModelFromMesh(mesh);
-      model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-      mesh_is_dirty = false;
-    }
 
     // Update objects
     StartPerformanceTracker("UpdateLoop");
     UpdatePlayer(&player);
     EndPerformanceTracker("UpdateLoop");
 
-<<<<<<< HEAD
-    // Block placement
-    StartPerformanceTracker("BlockPlacment");
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-      RemoveBlock(blocks, &player);
-      mesh_is_dirty = true;
-    }
-    if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
-      AddBlock(blocks, &player);
-      mesh_is_dirty = true;
-    }
-    EndPerformanceTracker("BlockPlacment");
-
-=======
->>>>>>> main
     // Rendering 3D
     BeginDrawing();
     ClearBackground(RAYWHITE);
     BeginMode3D(player.camera);
 
-<<<<<<< HEAD
-    StartPerformanceTracker("DrawingBlocks");
-    DrawModel(model, (Vector3){0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
-    EndPerformanceTracker("DrawingBlocks");
-=======
     DrawVoxel(voxels);
 
     DrawGrid(100, 1.0f);
->>>>>>> main
     EndMode3D();
 
     // Rendering 2D
@@ -86,7 +42,6 @@ int Scene3DGame(void) {
     EndPerformanceTracker("CompleteLoop");
   }
 
-  UnloadModel(model);
   CloseWindow();
 
   PrintPerformanceTrackers();
